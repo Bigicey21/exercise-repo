1a010d6f6fd0604702e480024afc714be169a2c8
Bigicey21 <icewolf21@mail.bg>
2016-09-20 14:23:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j= 1;  j < n;  j++)
+                {
+                    for (int k ='a'; k < 'a' + k; k++)
+                    {
+                        for (int l..
+                        {
+                            for (int m..
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
